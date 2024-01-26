80f611edc7bc96ce422d755a670537e6a6e98a15
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-01-26 16:44:16 +0530

    Even Odd Program & Leap Year Code Added

---
+++ operators_expressions/evenOdd.js
@@ -0,0 +1,9 @@
+// Number is even or odd
+
+const num = 5;
+
+if (num % 2 == 0) {
+    console.log('Even');
+} else {
+    console.log('Odd');
+}
---
+++ operators_expressions/leapYear.js
@@ -0,0 +1,11 @@
+// Leap Year Code
+
+const year = 2020;
+
+// change the year to see the changes
+
+if (year % 4 == 0) {
+    console.log('Leap Year');
+} else {
+    console.log('Not Leap Year');
+}
