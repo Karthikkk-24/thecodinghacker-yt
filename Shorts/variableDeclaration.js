17d20e6782b8f2af8cbf672cc92673383be089ac
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-03-12 07:33:19 +0530

    Variable Declaration Code

---
+++ Shorts/variableDeclaration.js
@@ -0,0 +1,22 @@
+// Variable Declaration In JavaScript
+
+// Variables
+
+// Var Keyword
+var name = "thecodinghacker";
+console.log(name); // thecodinghacker
+name = "javascript";
+console.log(name); // javascript
+
+// Let Keyword
+let name2 = "javascript";
+console.log(name2); // thecodinghacker
+name2 = "typescript";
+console.log(name2); // javascript
+
+// Constants
+const age = 21;
+console.log(age);
+
+age = 22;
+console.log(age); // error: redeclaring of constants
